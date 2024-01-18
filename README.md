0c9ca17ab077a36415e1966d7a079ca1b6bda51e
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-18 14:35:03 +0100

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Request Header Parser Microservice
+A Request Header Parser Microservice is a small web service or application designed to parse and analyze HTTP request headers sent by clients (typically web browsers or other HTTP clients). When a client makes an HTTP request to a server, it includes various headers that provide information about the request, the client, and the communication preferences
 
 This is the boilerplate for the Request Header Parser Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
--- index.js
+++ index.js
@@ -4,19 +4,27 @@
 // init project
 require('dotenv').config();
 var express = require('express');
+let bodyParser = require('body-parser')
 var app = express();
 
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json());
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/style', (req, res) => {
+  res.sendFile(__dirname + '/public/style.css');
 });
 
 // your first API endpoint...
